0fce0977553fc8431750bdec745ac30067b6521f
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-12-11 17:42:51 +0530

    Done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,50 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+  constructor() {
+    super()
+    this.state = {
+        currIndex: 0,
+    }
+  }
+  IncreaseCount = ()=>{
+    if (this.state.currIndex == (images.length - 1)) {
+        this.setState({
+            currIndex: 0,
+        })
+    }
+    else{this.setState({
+        currIndex: this.state.currIndex + 1
+    })}
+  }
+  DecreaseCount = ()=>{
+    if (this.state.currIndex == 0) {
+        this.setState({
+            currIndex: images.length - 1,
+        })
+    }
+    else{this.setState({
+        currIndex: this.state.currIndex - 1
+    })}
+  }
+  render(){
+    return (
+        <>
+            <h1>Carousel</h1>
+            <div className="carousel-container flex">
+                <div className="left-arrow arrowDiv flex" onClick={this.DecreaseCount}>
+                    <ArrowBackIosIcon/> 
+                </div>
+                <h2 className="title">{images[this.state.currIndex].title}</h2>
+                <img src={images[this.state.currIndex].img} alt="" />
+                <h4 className="subtitle">{images[this.state.currIndex].subtitle}</h4>
+                <div className="right-arrow arrowDiv flex" onClick={this.IncreaseCount}>
+                    <ArrowForwardIosIcon/>
+                </div>
+            </div>
+        </>
+    )
+  }
 }
 
 export default Carousel;
